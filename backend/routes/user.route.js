2663e98329d0b4df14c3039dde08572139f8f79c
Samapti <samaptigun@gmail.com>
2026-02-19 17:47:49 +0530

    Get user api route

---
+++ backend/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from "express"
+import { verifyToken } from "../utils/verifyUser.js"
+import { getUsers } from "../controllers/user.contoller.js"
+const router = express.Router()
+
+router.get("/getusers",verifyToken,getUsers)
+
+export default router
