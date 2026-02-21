d0801c4a512915580c569c4a6356500b7ae124b2
Samapti <samaptigun@gmail.com>
2026-02-21 13:01:08 +0530

    image uploading api route

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import cookieParser  from "cookie-parser"
+import path from "path"
 
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import travelStoryRoutes from "./routes/travelStory.route.js"
+import { fileURLToPath } from "url"
 
 dotenv.config()
 
@@ -24,6 +26,7 @@ app.use(cookieParser())
 //for allowing json object in req body as middleware
 app.use(express.json())
 
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000!")
 })
@@ -31,6 +34,12 @@ app.listen(3000,()=>{
 app.use("/api/auth",authRoutes)
 app.use("/api/user",userRoutes)
 app.use("/api/travel-story",travelStoryRoutes)
+
+//serve static files from the uploads and assets directory
+const __filename=fileURLToPath(import.meta.url)
+const __dirname= path.dirname(__filename)
+
+app.use("/uploads",express.static(path.join(__dirname,"uploads")))
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500
