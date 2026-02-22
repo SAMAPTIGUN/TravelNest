7157f4e06bc299938943a1cd1251e2c0ab674ebc
Samapti <samaptigun@gmail.com>
2026-02-22 08:45:17 +0530

    deiete story api route

--- backend/controllers/travelStory.controller.js
+++ backend/controllers/travelStory.controller.js
@@ -139,3 +139,34 @@ export const editTravelStory = async (req, res, next) => {
     next(error)
   }
 }
+
+export const deleteTravelStory=async(req,res,next)=>{
+  const {id}=req.params
+  const userId=req.user.id
+ try {
+    const travelStory = await TravelStory.findOne({ _id: id, userId: userId })
+
+    if (!travelStory) {
+      next(errorHandler(404, "Travel Story not found!"))
+    }
+
+    // delete travel story from the database
+    await travelStory.deleteOne({ _id: id, userId: userId })
+
+    // Extract the filename from the imageUrl
+    const imageUrl = travelStory.imageUrl
+      const filename = path.basename(imageUrl)
+      
+      const filePath = path.join(rootDir, "uploads", filename)
+
+      
+    if (!fs.existsSync(filePath)) {
+        
+        return next(errorHandler(404,"Image not found"))
+    }
+    await fs.promises.unlink(filePath)
+    res.status(200).json({ message: "Travel story deleted successfully!" })
+  } catch (error) {
+    next(error)
+  }
+}
