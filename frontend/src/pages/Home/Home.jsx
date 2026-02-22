036e888fc7fa419fbfa8c6a1bf8960da2362c9c2
Samapti <samaptigun@gmail.com>
2026-02-22 17:44:44 +0530

    login ui

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 
 const Home = () => {
+  // Get all travel stories
+  const getAllTravelStories = async () => {
+    try {
+      const response = await axiosInstance.get("/travel-story/get-all")
+
+      if (response.data && response.data.stories) {
+        setAllStories(response.data.stories)
+      }
+    } catch (error) {
+      console.log("Something went wrong. Please try again.")
+    }
+  }
   return (
-    <div>
-      Home
+    <>
+     <Navbar
+      />
+    <div className="container mx-auto py-10">
+      <div className="flex gap-7">
+        <div className="flex-1">
+        </div>
+
+         <div className="w-[320px]">
+
+         </div>
+      </div>
     </div>
+    </>
+    
   )
 }
 
