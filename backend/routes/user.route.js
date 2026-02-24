fd1d29d6592fb14c621fcc64b458e717a70fd9e0
Samapti <samaptigun@gmail.com>
2026-02-24 23:57:47 +0530

    signout api route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,12 @@
 import express from "express"
+
 import { verifyToken } from "../utils/verifyUser.js"
-import { getUsers } from "../controllers/user.contoller.js"
+import { getUsers, signout } from "../controllers/user.contoller.js"
+
 const router = express.Router()
 
-router.get("/getusers",verifyToken,getUsers)
+router.get("/getusers", verifyToken, getUsers)
+
+router.post("/signout", signout)
 
 export default router
