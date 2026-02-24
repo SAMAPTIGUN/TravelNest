fe9b6e4d2a8b12664efeeb50e4646c83547edfdb
Samapti <samaptigun@gmail.com>
2026-02-24 14:37:47 +0530

    Navbar ui

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,35 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
 
 const Home = () => {
-  // Get all travel stories
-  const getAllTravelStories = async () => {
-    try {
-      const response = await axiosInstance.get("/travel-story/get-all")
-
-      if (response.data && response.data.stories) {
-        setAllStories(response.data.stories)
-      }
-    } catch (error) {
-      console.log("Something went wrong. Please try again.")
-    }
-  }
-  return (
-    <>
-     <Navbar
-      />
-    <div className="container mx-auto py-10">
-      <div className="flex gap-7">
-        <div className="flex-1">
-        </div>
-
-         <div className="w-[320px]">
-
-         </div>
-      </div>
-    </div>
-    </>
-    
-  )
+  return <>
+  <Navbar/>
+  </>
+  
 }
 
 export default Home
