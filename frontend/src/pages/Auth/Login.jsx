7cb5e8a467824dd017d3d365b16986612d119f72
Samapti <samaptigun@gmail.com>
2026-03-01 18:34:12 +0530

    complete project

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -3,32 +3,41 @@ import PasswordInput from "../../components/PasswordInput"
 import { useNavigate } from "react-router-dom"
 import axiosInstance from "../../utils/axiosInstance"
 import { validateEmail } from "../../utils/hlper"
-import { useDispatch,useSelector } from "react-redux"
-import {signInStart,signInSuccess} from "../../redux/slice/userSlice"
+import { useDispatch, useSelector } from "react-redux"
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../../redux/slice/userSlice"
+
 const Login = () => {
-   const navigate = useNavigate()
-     const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
   const [email, setEmail] = useState("")
-   const [password, setPassword] = useState("")
-     const [error, setError] = useState("")
-      const { loading} = useSelector((state) => state.user)
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
 
-     const handleSubmit = async (e) => {
-      e.preventDefault()
+  const { loading, currentUser } = useSelector((state) => state.user)
 
-      if (!validateEmail(email)) {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!validateEmail(email)) {
       setError("Please enter a valid email address.")
       return
     }
-     if (!password) {
+
+    if (!password) {
       setError("Please enter your password.")
       return
     }
+
     setError(null)
 
     // Login API call
     try {
-     dispatch(signInStart())
+      dispatch(signInStart())
 
       const response = await axiosInstance.post("/auth/signin", {
         email,
@@ -38,9 +47,12 @@ const Login = () => {
       if (response.data) {
         dispatch(signInSuccess(response.data))
         navigate("/")
+      } else {
+        dispatch(signInFailure("An unexpected error occurred!"))
       }
-      
     } catch (error) {
+      dispatch(signInFailure("An unexpected error occurred!"))
+
       if (
         error.response &&
         error.response.data &&
@@ -50,43 +62,52 @@ const Login = () => {
       } else {
         setError("Something went wrong. Please try again.")
       }
-      
     }
   }
-  
-    
-     
+
+  useEffect(() => {
+    if (!loading && currentUser) {
+      navigate("/")
+    }
+  }, [currentUser])
+
   return (
     <div className="h-screen bg-cyan-50 overflow-hidden relative">
       <div className="login-ui-box right-10 -top-40" />
+
       <div className="container h-screen flex items-center justify-center px-20 mx-auto">
-      <div className="w-2/4 h-[90vh] flex items-end bg-[url('https://images.pexels.com/photos/731217/pexels-photo-731217.jpeg?auto=compress&cs=tinysrgb&w=600')] bg-cover bg-center rounded-lg p-10 z-50">
-      <div>
+        <div className="w-2/4 h-[90vh] flex items-end bg-[url('https://images.pexels.com/photos/731217/pexels-photo-731217.jpeg?auto=compress&cs=tinysrgb&w=600')] bg-cover bg-center rounded-lg p-10 z-50">
+          <div>
             <h4 className="text-5xl text-white font-semibold leading-14.5">
-              Create Your <br /> Travel Memories
+              Create Your <br /> Travel Stories
             </h4>
 
             <p className="text-[15px] text-white leading-6 pr-7 mt-4">
               Record your travel experiences and memories in your travel journey
             </p>
           </div>
-      </div>
-      <div className="w-2/4 h-[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/20">
+        </div>
+
+        <div className="w-2/4 h-[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/20">
           <form onSubmit={handleSubmit}>
             <h4 className="text-2xl font-semibold mb-7">Login</h4>
-             <input
+
+            <input
               type="email"
               placeholder="Email"
               className="input-box"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-            <PasswordInput value={password}
+
+            <PasswordInput
+              value={password}
               onChange={(e) => {
                 setPassword(e.target.value)
-              }}/>
+              }}
+            />
 
-              {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
             {loading ? (
               <p className="animate-pulse w-full text-center btn-primary">
@@ -97,16 +118,18 @@ const Login = () => {
                 LOGIN
               </button>
             )}
-              <p className="text-xs text-slate-500 text-center my-4">Or</p>
+
+            <p className="text-xs text-slate-500 text-center my-4">Or</p>
+
             <button
               type="submit"
               className="btn-primary btn-light"
-               onClick={() => navigate("/sign-up")}
+              onClick={() => navigate("/sign-up")}
             >
               CREATE ACCOUNT
             </button>
-            </form>
-      </div>
+          </form>
+        </div>
       </div>
     </div>
   )
